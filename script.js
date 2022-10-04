d4da68e0e4e6d80dca40b34b814d6da7ab31c780
MR. ANDOH FRANCIS <andohfrancis9187@gmail.com>
2022-10-03 21:41:41 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function showwatico(){
+    let x = document.getElementById("watico")
+    if (x.style.display === "none"){
+        x.style.display = "block"
+
+    }
+    else{
+        
+        x.style.display = "none"
+      
+        
+    }
+
+}
+
+
+
